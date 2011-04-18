9f2e970afc9ee53c43b6362cb926d1ae4cd26d35
Dermot Daly <dermot.daly@tapadoo.com>
2011-04-18 13:38:39 +0100

    Removed echo

--- listdevs.sh
+++ listdevs.sh
@@ -7,7 +7,6 @@ TMPDIR=tmp$$.tmp
 mkdir $TMPDIR
 unzip -qq $IPAFILE -d $TMPDIR
 EMBEDFILE=`find $TMPDIR -name embedded.mobileprovision -print | head -1`
-echo $EMBEDFILE
 EMBEDFILEONLY=$EMBEDFILE.plist
 sed -n '/plist/,/\/plist/p' $EMBEDFILE > $EMBEDFILEONLY
 sed -n '/ProvisionedDevices/,/\/array/p' $EMBEDFILEONLY | grep string | sed 's/<[/]*string>//g'
