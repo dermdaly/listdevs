378f4652e91c56e3f7f1d36b0da965673579056b
Johannes la Poutre <info@squio.nl>
2013-10-07 10:38:29 +0200

    Allow for spaces in paths / filenames
    Quoted all Paths and Filenames to work with projects and App names with
    spaces in their file names.

--- listdevs.sh
+++ listdevs.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Simple script to list the devices an ipa has been built for
 # Copyright © Tapadoo, 2011.  All Rights Reserved
@@ -6,13 +6,12 @@
 # You are free to modify this for your needs 
 LANG=C
 LC_CTYPE=C
-IPAFILE=$1
+IPAFILE="$1"
 TMPDIR=tmp$$.tmp
 mkdir $TMPDIR
-unzip -qq $IPAFILE -d $TMPDIR
+unzip -qq "$IPAFILE" -d $TMPDIR
 EMBEDFILE=`find $TMPDIR -name embedded.mobileprovision -print | head -1`
-EMBEDFILEONLY=$EMBEDFILE.plist
-sed -n '/plist/,/\/plist/p' $EMBEDFILE > $EMBEDFILEONLY
-sed -n '/ProvisionedDevices/,/\/array/p' $EMBEDFILEONLY | grep string | sed 's/<[/]*string>//g'
+EMBEDFILEONLY="${EMBEDFILE}.plist"
+sed -n '/plist/,/\/plist/p' "$EMBEDFILE" > "$EMBEDFILEONLY"
+sed -n '/ProvisionedDevices/,/\/array/p' "$EMBEDFILEONLY" | grep string | sed 's/<[/]*string>//g'
 rm -rf $TMPDIR
-
